5dadfc02ca9198c34727f72bf8750fafb517acca
Frank Meyer <caladan@me.com>
2017-09-10 01:21:13 +0200

    Working on EntriesController and view for all entries

--- src/main/java/de/meyer/kronik/controller/EntriesController.java
+++ src/main/java/de/meyer/kronik/controller/EntriesController.java
@@ -1,12 +1,33 @@
 package de.meyer.kronik.controller;
 
 
+import de.meyer.kronik.repositories.EntryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/entries/")
 public class EntriesController {
+
+    private EntryRepository er;
+
+    @Autowired
+    public void setEr(EntryRepository er) {
+        this.er = er;
+    }
+
+    @RequestMapping("/entries")
+    public String showEntries(Model model) {
+
+        model.addAllAttributes("entries", er.getAllEntries());
+
+        return "showEntries";
+
+
+    }
+
+
 
 
 
