f67b3d986edf39c5f334584c57905a45628a434c
Frank Meyer <caladan@me.com>
2017-09-09 23:04:46 +0200

    Customizing Error Message. Creating Entry List View.

--- src/main/java/de/meyer/kronik/repositories/EntryRepository.java
+++ src/main/java/de/meyer/kronik/repositories/EntryRepository.java
@@ -2,8 +2,11 @@ package de.meyer.kronik.repositories;
 
 import de.meyer.kronik.entities.Entry;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface EntryRepository extends JpaRepository<Entry, Long> {
+public interface EntryRepository extends CrudRepository<Entry, Long> {
+
+
 
 
 
